5d12087febd208f1de18c6a0919c51b66c0a8580
Thet Hmuu Eain Soe <thethmuueinsoe@gmail.com>
2022-11-14 18:16:41 +0630

    initial state

--- src/App.js
+++ src/App.js
@@ -39,62 +39,37 @@ export default function App() {
 
   return (
     <div className='App'>
-      <motion.h1
-        initial={{ opacity: 0, y: -200 }}
-        animate={{ opacity: 1, y: 0 }}
-      >
-        Skill List
-      </motion.h1>
+      <h1>Skill List</h1>
 
-      <motion.form
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 1 }}
-        onSubmit={addSkill}
-      >
+      <form onSubmit={addSkill}>
         <input className='input' type='text' ref={inputRef} required />
         <button className='add-btn' type='submit'>
           Add
         </button>
-      </motion.form>
+      </form>
 
       <SkillList skills={skills} removeSkill={removeSkill} />
     </div>
   );
 }
 
-const itemVariants = {
-  hidden: { opacity: 0, x: -200 },
-  visible: { opacity: 1, x: 0 },
-  transition: { duration: 2 },
-};
-
 function SkillList({ skills, removeSkill }) {
   return (
     <ul className='skill-list'>
-      <AnimatePresence>
-        {skills.length > 0 ? (
-          skills.map((skill) => (
-            <motion.li
-              variants={itemVariants}
-              initial='hidden'
-              animate='visible'
-              exit='hidden'
-              className='skill-item'
-              key={skill.id}
-            >
-              {skill.name}
-              <button className='del-btn' onClick={() => removeSkill(skill.id)}>
-                <span className='del-icon' role='img' aria-label='cross icon'>
-                  ❎
-                </span>
-              </button>
-            </motion.li>
-          ))
-        ) : (
-          <p>no item currently...</p>
-        )}
-      </AnimatePresence>
+      {skills.length > 0 ? (
+        skills.map((skill) => (
+          <li className='skill-item' key={skill.id}>
+            {skill.name}
+            <button className='del-btn' onClick={() => removeSkill(skill.id)}>
+              <span className='del-icon' role='img' aria-label='cross icon'>
+                ❎
+              </span>
+            </button>
+          </li>
+        ))
+      ) : (
+        <p>no item currently...</p>
+      )}
     </ul>
   );
 }
